50c617d6823423a88fce150b9e29708463266cdb
dni ⚡ <office@dnilabs.com>
2024-04-22 14:57:07 +0200

    feat: add lnurlauth `auth()`
    taken from `lnbits/lnbits` repository

--- lnurl/core.py
+++ lnurl/core.py
@@ -4,9 +4,9 @@ import requests
 from pydantic import ValidationError
 
 from .exceptions import InvalidLnurl, InvalidUrl, LnurlResponseException
-from .helpers import _url_encode
-from .models import LnurlAuthResponse, LnurlResponse, LnurlResponseModel
-from .types import ClearnetUrl, DebugUrl, LnAddress, Lnurl, OnionUrl
+from .helpers import encode_strict_der, lnurlauth_key, url_encode
+from .models import LnurlResponse, LnurlResponseModel
+from .types import ClearnetUrl, DebugUrl, Lnurl, OnionUrl
 
 
 def decode(bech32_lnurl: str) -> Union[OnionUrl, ClearnetUrl, DebugUrl]:
@@ -18,27 +18,30 @@ def decode(bech32_lnurl: str) -> Union[OnionUrl, ClearnetUrl, DebugUrl]:
 
 def encode(url: str) -> Lnurl:
     try:
-        return Lnurl(_url_encode(url))
+        return Lnurl(url_encode(url))
     except (ValidationError, ValueError):
         raise InvalidUrl
 
 
 def get(url: str, *, response_class: Optional[Any] = None, verify: Union[str, bool] = True) -> LnurlResponseModel:
     try:
-        r = requests.get(url, verify=verify)
-        r.raise_for_status()
+        req = requests.get(url, verify=verify)
+        req.raise_for_status()
     except Exception as e:
         raise LnurlResponseException(str(e))
 
     if response_class:
         assert issubclass(response_class, LnurlResponseModel), "Use a valid `LnurlResponseModel` subclass."
-        return response_class(**r.json())
+        return response_class(**req.json())
 
-    return LnurlResponse.from_dict(r.json())
+    return LnurlResponse.from_dict(req.json())
 
 
 def handle(
-    bech32_lnurl: str, *, response_class: Optional[LnurlResponseModel] = None, verify: Union[str, bool] = True
+    bech32_lnurl: str,
+    response_class: Optional[LnurlResponseModel] = None,
+    verify: Union[str, bool] = True,
+    login_id: Optional[str] = None,
 ) -> LnurlResponseModel:
     try:
         if "@" in bech32_lnurl:
@@ -49,6 +52,42 @@ def handle(
         raise InvalidLnurl
 
     if lnurl.is_login:
-        return LnurlAuthResponse(callback=lnurl.url, k1=lnurl.url.query_params["k1"])
+        try:
+            return auth(lnurl, login_id=login_id, verify=verify)
+        except (ValidationError, ValueError):
+            raise InvalidLnurl
 
     return get(lnurl.url, response_class=response_class, verify=verify)
+
+
+def auth(
+    lnurl: Lnurl,
+    response_class: Optional[Any] = None,
+    login_id: Optional[str] = None,
+    verify: Union[str, bool] = True,
+) -> LnurlResponseModel:
+    k1 = bytes.fromhex(lnurl.url.query_params["k1"])
+    assert login_id, "Provide a valid login_id to sign the message."
+    auth_key = lnurlauth_key(lnurl.url.netloc, login_id)
+    sig = auth_key.sign_digest_deterministic(k1, sigencode=encode_strict_der)
+    assert auth_key.verifying_key, "LNURLauth verifying_key does not exist"
+    try:
+        req = requests.get(
+            lnurl.url,
+            verify=verify,
+            params={
+                "k1": k1.hex(),
+                "key": auth_key.verifying_key.to_string("compressed").hex(),
+                "sig": sig.hex(),
+            },
+        )
+        req.raise_for_status()
+    except Exception as e:
+        raise LnurlResponseException(str(e))
+
+    if response_class:
+        assert issubclass(response_class, LnurlResponseModel), "Use a valid `LnurlResponseModel` subclass."
+        return response_class(**req.json())
+
+    return LnurlResponse.from_dict(req.json())
+    # return LnurlErrorResponse(reason=withdraw.reason)
