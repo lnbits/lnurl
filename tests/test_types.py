3778dc2962b11d8cf837340e748afc8b5578149c
dni ⚡ <office@dnilabs.com>
2025-09-03 14:38:47 +0200

    fix: http urls on localhost would create the wrong https url
    - add tests

--- tests/test_types.py
+++ tests/test_types.py
@@ -199,6 +199,20 @@ class TestLnurl:
         with pytest.raises(ValidationError):
             parse_obj_as(Lnurl, bech32)
 
+    @pytest.mark.parametrize(
+        "url",
+        [
+            "lnurlp://localhost",
+            "http://localhost",
+        ],
+    )
+    def test_insecure_lnurl(self, url: str):
+        lnurl = parse_obj_as(Lnurl, url)
+        assert lnurl.url.insecure is True
+        assert lnurl.url.host == "localhost"
+        assert lnurl.url.startswith("http://")
+        assert lnurl.startswith("http://")
+
 
 class TestLnurlPayMetadata:
     @pytest.mark.parametrize(
